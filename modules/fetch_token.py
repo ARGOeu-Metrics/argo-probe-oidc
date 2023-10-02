180304a29acd841dd654a6935e1f6f3951e5c74a
Katarina Zailac <katarina.zailac@gmail.com>
2023-10-02 13:54:17 +0200

    Add 'user' parameter to fetch_token probe

--- modules/fetch_token.py
+++ modules/fetch_token.py
@@ -10,33 +10,39 @@ from argo_probe_oidc.NagiosResponse import NagiosResponse
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Nagios probe for fetching OIDC tokens."
+        description="ARGO probe for fetching OIDC tokens.",
     )
     parser.add_argument(
         "-u", "--url", dest="url", type=str,
         default="https://aai.egi.eu/oidc/token",
-        help="URL from which the token is fetched"
+        help="URL from which the token is fetched "
+             "(default: https://aai.egi.eu/oidc/token)"
     )
     parser.add_argument(
         "--client_id", dest="client_id", type=str, required=True,
-        help="The identifier of the client"
+        help="identifier of client"
     )
     parser.add_argument(
         "--client_secret", dest="client_secret", type=str, required=True,
-        help="The secret value of the client"
+        help="secret value of client"
     )
     parser.add_argument(
         "--refresh_token", dest="refresh_token", type=str, required=True,
-        help="The value of the refresh token"
+        help="refresh token"
     )
     parser.add_argument(
         "--token_file", dest="token_file", type=str,
         default="/etc/nagios/globus/oidc",
-        help="File for storing obtained token"
+        help="file for storing obtained token "
+             "(default: /etc/nagios/globus/oidc)"
+    )
+    parser.add_argument(
+        "-U", "--user", dest="user", type=str, default="nagios",
+        help="username of user executing the probe (default: nagios)"
     )
     parser.add_argument(
         "-t", "--timeout", dest="timeout", type=int, default=60,
-        help="timeout"
+        help="timeout in seconds (default: 60)"
     )
     args = parser.parse_args()
 
@@ -63,19 +69,19 @@ def main():
             f.write(access_token)
 
         try:
-            uid = pwd.getpwnam("nagios").pw_uid
+            uid = pwd.getpwnam(args.user).pw_uid
 
         except KeyError:
-            nagios.writeCriticalMessage("No user named 'nagios'")
+            nagios.writeCriticalMessage(f"No user named '{args.user}'")
             nagios.setCode(nagios.CRITICAL)
             print(nagios.getMsg())
             sys.exit(nagios.getCode())
 
         try:
-            gid = grp.getgrnam("nagios").gr_gid
+            gid = grp.getgrnam(args.user).gr_gid
 
         except KeyError:
-            nagios.writeCriticalMessage("No group named 'nagios'")
+            nagios.writeCriticalMessage(f"No group named '{args.user}'")
             nagios.setCode(nagios.CRITICAL)
             print(nagios.getMsg())
             sys.exit(nagios.getCode())
